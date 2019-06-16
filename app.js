aa0f36077f62cc9f32a0f5ddc637c88a37e39e7c
Josh Kern <jkern.dev@gmail.com>
2019-06-15 20:56:46 -0700

    bcrypt user registration

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const app = express();
+const mongoose = require("mongoose");
+const db = require("./config/keys").mongoURI;
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const bodyParser = require('body-parser');
+
+mongoose.connect(db, { useNewUrlParser: true})
+  .then(() => console.log("connected to Mongo"))
+  .catch(err => console.log(err));
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+app.use(bodyParser.json());
 
 app.get("/", (req,res) => {
   res.send("Hello Twitter!");
 });
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
 
 const port = process.env.PORT || 5000;
 
