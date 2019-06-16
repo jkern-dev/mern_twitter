306ddc146528b0903b31b238100d65466869f43b
Josh Kern <jkern.dev@gmail.com>
2019-06-15 23:57:42 -0700

    tweet front end

---
+++ frontend/src/components/main/main_page.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class MainPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Mern Twitter</h1>
+      </div>
+    );
+  }
+}
+
+export default MainPage;
